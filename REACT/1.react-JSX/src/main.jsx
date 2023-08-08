59a74546f7ce25ca1787ad211637215777a0687c
Faria Afrose <85243808+faria-afrose@users.noreply.github.com>
2023-08-08 21:09:03 +0600

    Add files via upload

---
+++ REACT/1.react-JSX/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+const jsToHtml = "This msg from js to jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h1>Hlw</h1>
+    <h2>{jsToHtml}</h2>
+    <p className="headingStyle">
+      Door only in curtain seat as, all saintly dreaming and raven open. I its
+      deep a muttered into the footfalls, croaking chamber more of chamber. Me
+      nevermore desert more myself.
+    </p>
+  </React.StrictMode>
+);
